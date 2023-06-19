7be29e97d8aa0f8bbc7e96943e31537d22cd6608
Jose Teodoro Ynga Chiroque <jose.ynga@gmail.com>
2023-06-19 07:13:32 -0500

    segundo avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,56 @@
-import { useEffect, useState } from "react"
-import Navbar from "./components/Navbar"
-import Stays from './components/Stays/Stays'
+import { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import Stays from "./components/Stays/Stays";
 
 function App() {
-  const [city, setCity] = useState(0)
-  const [guests, setGuests] = useState(0)
+  const [city, setCity] = useState(0);
+  const [guests, setGuests] = useState(0);
 
   const setSearch = (city, guests) => {
-    //creamos Array de ciudades 
+    //creamos Array de ciudades
     if (city != 0) {
-      const newCity = city.split(",")
-      setCity(newCity[0])
-    }
-    else setCity(0)
-    setGuests(guests)
-  }
-
-
+      const newCity = city.split(",");
+      setCity(newCity[0]);
+    } else setCity(0);
+    setGuests(guests);
+  };
 
   return (
     <div className="App" style={{ fontFamily: `Montserrat, sans-serif` }}>
       <Navbar search={setSearch} />
       <Stays city={city} guests={guests} />
     </div>
-  )
+  );
 }
 
-export default App
-
-
-
+export default App;
 
 //import { useEffect, useState } from "react";
 //import "./App.css";
 
 //function App() {
-  // La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
-  //const [data, setData] = useState([]);
+// La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
+//const [data, setData] = useState([]);
 
-  // Función para traer los datos de "stays.json".
-  //const getData = async () => {
-    // Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
-    //try {
-   //   const res = await fetch("stays.json");
-   //   const resJson = await res.json();
-      // Aquí guardamos los datos de "stays.json" en la variable data.
-  //    setData(resJson);
-  //  } catch (error) {
-  //    console.log(error);
- //   }
- // };
+// Función para traer los datos de "stays.json".
+//const getData = async () => {
+// Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
+//try {
+//   const res = await fetch("stays.json");
+//   const resJson = await res.json();
+// Aquí guardamos los datos de "stays.json" en la variable data.
+//    setData(resJson);
+//  } catch (error) {
+//    console.log(error);
+//   }
+// };
 
-  // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
-  //useEffect(() => {
-  //   getData();
- // }, []);
+// Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
+//useEffect(() => {
+//   getData();
+// }, []);
 
- /* // Puedes ver la variable data en consola.
+/* // Puedes ver la variable data en consola.
   //console.log(data);
  // return (
     <>
---
+++ src/Casa/House.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import StarIcon from "@mui/icons-material/Star"; //libreria de Iconos
+import "./House.css"; //css para casa
+
+const House = ({ house, handle }) => {
+  return (
+    <div
+      id="home"
+      className="home"
+      onClick={() => {
+        handle(house);
+      }}
+    >
+      <div id="homeImg" className="homeImg">
+        <img src={house.photo} alt="House img" />
+      </div>
+
+      <div id="homeInfo" className="homeInfo">
+        <div id="homeInfoHeader" className="homeInfoHeader">
+          {house.superHost && <div className="superHost">SUPER HOST</div>}
+          <div className="homeType">{house.type}</div>
+          <div className="homeStars">
+            <StarIcon style={{ color: `#EB5757` }} /> {house.rating}
+          </div>
+        </div>
+
+        <h4 className="homeTitle">{house.title}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default House;
